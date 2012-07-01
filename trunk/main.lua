9fb4ae32d8249c0a6a6e83ec8c47f7ee4ebf23ff
grdn <grdn@9b05a24d-1d3e-4892-bdf7-68edea2102f5>
2012-07-01 09:39:18 +0000

    Initial commit

---
+++ trunk/main.lua
@@ -0,0 +1,11 @@
+--
+-- !BetterFont
+-- written by Tobias Schulz (grdn) 2011, EU-Onyxia (Germany)
+--
+-- Little tweak which enables a new default font to your UI.
+--
+
+DAMAGE_TEXT_FONT = "Interface\\AddOns\\!BetterFont\\Prototype.ttf";
+UNIT_NAME_FONT   = "Interface\\AddOns\\!BetterFont\\Prototype.ttf";
+NAMEPLATE_FONT   = "Interface\\AddOns\\!BetterFont\\Prototype.ttf";
+STANDARD_TEXT_FONT = "Interface\\AddOns\\!BetterFont\\Prototype.ttf";
